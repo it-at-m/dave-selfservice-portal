ae8e24e4e7ab4b1c46d95860d3ea1e1ccbb75815
boal <56300801+boal@users.noreply.github.com>
2024-09-05 10:35:42 +0200

    DAVE-304 Refactor Hebung Frameworks Java (#31)
    * adapt classes and pom
    
    * change priofile for test
    
    * remove zipkin
    
    * remove dev
    
    * remove
    
    * remove
    
    * add context propagation
    
    * add profiles in hazelcast config
    
    * hebung spring boot
    
    * change node version in workflows

--- apigateway/src/main/java/de/muenchen/dave/configuration/NoSecurityConfiguration.java
+++ apigateway/src/main/java/de/muenchen/dave/configuration/NoSecurityConfiguration.java
@@ -1,12 +1,13 @@
 /*
  * Copyright (c): it@M - Dienstleister für Informations- und Telekommunikationstechnik
- * der Landeshauptstadt München, 2021
+ * der Landeshauptstadt München, 2023
  */
 package de.muenchen.dave.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -17,12 +18,9 @@ public class NoSecurityConfiguration {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(final ServerHttpSecurity http) {
         return http
-                .authorizeExchange()
-                .anyExchange().permitAll()
-                .and()
-                .cors()
-                .and()
-                .csrf().disable()
+                .authorizeExchange(exchange -> exchange.anyExchange().permitAll())
+                .cors(Customizer.withDefaults())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .build();
     }
 
