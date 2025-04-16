3ebacd6b7c1aae3f8946e0bdf127f6b0caa43a3c
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2025-04-16 15:48:48 +0200

    change to composition api

--- frontend/src/components/icons/QuelleIcon.vue
+++ frontend/src/components/icons/QuelleIcon.vue
@@ -4,7 +4,7 @@
         :color="color"
         :icon="icon.iconPath"
         :tooltip="icon.tooltip"
-    ></base-icon>
+    />
 </template>
 <script setup lang="ts">
 import BaseIcon from "@/components/icons/TooltipWithIcon.vue";
--- frontend/src/components/icons/WetterIcon.vue
+++ frontend/src/components/icons/WetterIcon.vue
@@ -4,69 +4,67 @@
         :color="color"
         :icon="icon.iconPath"
         :tooltip="icon.tooltip"
-    ></base-icon>
+    />
 </template>
-<script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+<script setup lang="ts">
 import BaseIcon from "@/components/icons/TooltipWithIcon.vue";
 import IconOptions from "@/components/icons/IconOptions";
 import Wetter from "@/domain/enums/Wetter";
+import { computed } from "vue";
 
-@Component({
-    components: {
-        BaseIcon,
-    },
-})
-export default class WetterIcon extends Vue {
-    @Prop({ default: false }) small?: boolean;
-    @Prop({ default: "black" }) color?: string;
-    @Prop() wetter!: string;
+interface Props {
+    small?: boolean;
+    color?: string;
+    wetter?: string;
+}
 
-    /**
-     * Lädt das richtige MDI Icon aus der Liste.
-     */
-    get icon(): IconOptions {
-        let result = WetterIcon.wetterIcons().get(this.wetter);
-        if (result === undefined) {
-            result = new IconOptions(
-                "mdi-cloud-question",
-                "Keine Information zum Wetter"
-            );
-        }
-        return result;
+const props = withDefaults(defineProps<Props>(), {
+    small: false,
+    color: "black",
+    wetter: "",
+});
+
+/**
+ * Lädt das richtige MDI Icon aus der Liste.
+ */
+const icon = computed<IconOptions>(() => {
+    let result = wetterIcons().get(props.wetter);
+    if (result === undefined) {
+        result = new IconOptions(
+            "mdi-cloud-question",
+            "Keine Information zum Wetter"
+        );
     }
+    return result;
+});
 
-    /**
-     * Alle Wetter Icons zu den Schlüsseln.
-     */
-    static wetterIcons(): Map<string, IconOptions> {
-        return new Map([
-            [Wetter.SUNNY, new IconOptions("mdi-weather-sunny", "Sonnig")],
-            [
-                Wetter.SUNNY_COLD,
-                new IconOptions("mdi-weather-hazy", "Sonnig, kalt"),
-            ],
-            [
-                Wetter.CLOUDY,
-                new IconOptions("mdi-weather-partly-cloudy", "Bewölkt"),
-            ],
-            [
-                Wetter.RAINY,
-                new IconOptions("mdi-weather-rainy", "Regnerisch (Schauer)"),
-            ],
-            [
-                Wetter.CONTINUOUS_RAINY,
-                new IconOptions(
-                    "mdi-weather-pouring",
-                    "Regnerisch (dauerhaft)"
-                ),
-            ],
-            [Wetter.FOGGY, new IconOptions("mdi-weather-fog", "Neblig")],
-            [
-                Wetter.SNOWY,
-                new IconOptions("mdi-weather-snowy-heavy", "Schneefall"),
-            ],
-        ]);
-    }
+/**
+ * Alle Wetter Icons zu den Schlüsseln.
+ */
+function wetterIcons(): Map<string, IconOptions> {
+    return new Map([
+        [Wetter.SUNNY, new IconOptions("mdi-weather-sunny", "Sonnig")],
+        [
+            Wetter.SUNNY_COLD,
+            new IconOptions("mdi-weather-hazy", "Sonnig, kalt"),
+        ],
+        [
+            Wetter.CLOUDY,
+            new IconOptions("mdi-weather-partly-cloudy", "Bewölkt"),
+        ],
+        [
+            Wetter.RAINY,
+            new IconOptions("mdi-weather-rainy", "Regnerisch (Schauer)"),
+        ],
+        [
+            Wetter.CONTINUOUS_RAINY,
+            new IconOptions("mdi-weather-pouring", "Regnerisch (dauerhaft)"),
+        ],
+        [Wetter.FOGGY, new IconOptions("mdi-weather-fog", "Neblig")],
+        [
+            Wetter.SNOWY,
+            new IconOptions("mdi-weather-snowy-heavy", "Schneefall"),
+        ],
+    ]);
 }
 </script>
