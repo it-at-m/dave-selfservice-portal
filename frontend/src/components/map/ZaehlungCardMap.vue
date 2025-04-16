c3fd85b61a0fbbf1e0fd5bf3ab0c1bd69ec8ae30
alexander.kerscher <alexander.kerscher@muenchen.de>
2025-04-16 16:05:19 +0200

    map umgestellt

--- frontend/src/components/map/ZaehlungCardMap.vue
+++ frontend/src/components/map/ZaehlungCardMap.vue
@@ -4,213 +4,192 @@
         :width="width"
         :min-height="minheight"
     >
-        <l-map
-            ref="zaehlungcardmap"
-            :options="mapOptions"
-            style="z-index: 1"
-            @ready="mapReady"
-        >
-            <l-wms-tile-layer
-                base-url="https://geoportal.muenchen.de/geoserver/gsm/wms?"
-                layers="gsm:g_luftbild"
-                :visible="showLuftbild"
-                name="Luftbild"
-                attribution='&copy; <a href="https://www.muenchen.de/rathaus/Stadtverwaltung/Kommunalreferat/geodatenservice/geobasisdaten.html">GeodatenService München</a>'
-                layer-type="base"
-            />
-            <l-wms-tile-layer
-                base-url="https://geoportal.muenchen.de/geoserver/gsm/wms?"
-                layers="gsm:g_stadtkarte_gesamt"
-                :visible="!showLuftbild"
-                name="Stadtkarte"
-                attribution='&copy; <a href="https://www.muenchen.de/rathaus/Stadtverwaltung/Kommunalreferat/geodatenservice/geobasisdaten.html">GeodatenService München</a>'
-                layer-type="base"
-            />
-        </l-map>
+        <div
+            id="cardmap"
+            ref="cardmapRef"
+            :style="mapStyle"
+        />
     </v-sheet>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
-import { Component, Prop, Ref, Watch } from "vue-property-decorator";
-// imports for leaflet
-import {
-    LCircleMarker,
-    LControlLayers,
-    LIcon,
-    LLayerGroup,
-    LMap,
-    LMarker,
-    LTileLayer,
-    LTooltip,
-    LWMSTileLayer,
-} from "vue2-leaflet";
-// Api
-/* eslint-disable no-unused-vars */
-import {
+<script setup lang="ts">
+import L, {
     CircleMarker,
     CircleMarkerOptions,
-    control,
     Icon,
     LatLng,
-    MapOptions,
     Marker,
     MarkerOptions,
 } from "leaflet";
+import {
+    computed,
+    ComputedRef,
+    onBeforeUnmount,
+    onMounted,
+    ref,
+    watch,
+} from "vue";
 import markerIconRed from "@/assets/marker-icon-red.png";
 import { useEventbusStore } from "@/store/EventbusStore";
-/* eslint-enable no-unused-vars */
 
-@Component({
-    components: {
-        LIcon,
-        LMap,
-        LTileLayer,
-        LMarker,
-        LCircleMarker,
-        LTooltip,
-        LControlLayers,
-        LLayerGroup,
-        "l-wms-tile-layer": LWMSTileLayer,
-    },
-})
-export default class ZaehlungCardMap extends Vue {
-    @Prop({ default: "15vh" })
-    readonly height!: string;
+interface Props {
+    height?: string;
+    width?: string;
+    minheight?: string;
+    editZaehlungMarker?: boolean;
+    showLuftbild: boolean;
+    latLngZaehlstelle: LatLng;
+    latLngZaehlung: LatLng;
+}
 
-    @Prop({ default: "160px" })
-    readonly minheight!: string;
+const props = withDefaults(defineProps<Props>(), {
+    height: "100%",
+    width: "100%",
+    minheight: "160px",
+    editZaehlungMarker: false,
+});
 
-    @Prop({ default: "100%" })
-    readonly width!: string;
+const mapAttribution =
+    '&copy; <a href="https://stadt.muenchen.de/infos/geobasisdaten.html">GeodatenService München</a>';
 
-    @Prop()
-    private readonly latLngZaehlstelle!: LatLng;
-    @Prop()
-    private readonly latLngZaehlung!: LatLng;
+const cardmapRef = ref<HTMLDivElement | null>(null);
 
-    @Prop({ default: true })
-    readonly showLuftbild!: boolean;
+let cardmap: L.Map;
+let markerZaehlstelle: L.CircleMarker | null;
+let markerZaehlung: L.Marker | null;
 
-    @Prop({ default: false })
-    private readonly editZaehlungMarker!: boolean;
+const eventbus = useEventbusStore();
 
-    @Ref("zaehlungcardmap")
-    private readonly theMap!: LMap;
+watch(
+    () => eventbus.getResetFormEvent,
+    () => {
+        resetMap();
+    }
+);
 
-    private zoom = 16;
+onMounted(() => {
+    initMap();
+});
 
-    private markerZaehlstelle: CircleMarker | null = null;
+onBeforeUnmount(() => {
+    cardmap.remove();
+});
 
-    private markerZaehlung: Marker | null = null;
-
-    private eventbusStore = useEventbusStore();
-
-    /**
-     * Optionen fuer die Darstellung der Karte
-     */
-    mapOptions: MapOptions = {
+function initMap(): void {
+    cardmap = L.map(cardmapRef.value as HTMLElement, {
         minZoom: 10,
         maxZoom: 18,
+        zoom: 16,
         preferCanvas: false,
-        zoomControl: false,
         attributionControl: false,
-    };
+        zoomControl: false,
+        center: props.latLngZaehlstelle,
+    });
 
-    get resetFormEvent(): boolean {
-        return this.eventbusStore.getResetFormEvent;
-    }
-
-    @Watch("resetFormEvent")
-    private resetForm() {
-        this.zoom = 16;
-        this.resetMap();
-    }
-
-    private resetMap() {
+    cardmap.whenReady(() => {
         setTimeout(() => {
-            this.deleteMarkers();
-            if (this.latLngZaehlstelle) {
-                this.createMarkerForZaehlstelle(this.latLngZaehlstelle);
-            }
-            if (this.latLngZaehlung) {
-                this.createMarkerForZaehlung(this.latLngZaehlung);
-            }
-            this.theMap.mapObject?.setView(this.latLngZaehlstelle, this.zoom);
-        }, 200);
-    }
+            cardmap.invalidateSize();
+            createLayersAndAddToMap();
+            resetMap();
+        }, 10);
+    });
+}
 
-    // Legt einen neuen Marker für die Zählstelle an
-    private createMarkerForZaehlstelle(coords: LatLng) {
-        let options: CircleMarkerOptions = {} as CircleMarkerOptions;
-        options.opacity = 1.0;
-        options.radius = 20;
+function createLayersAndAddToMap(): void {
+    const baseLayers = createBaseLayers();
+    baseLayers.BaseLayer.addTo(cardmap);
+}
 
-        this.markerZaehlstelle = new CircleMarker(coords, options);
-
-        this.markerZaehlstelle.bindTooltip("Zählstelle", {
-            direction: "right",
-            offset: [options.radius, 0],
-        });
-
-        this.markerZaehlstelle.addTo(this.theMap.mapObject);
-    }
-
-    // Legt einen neuen Marker für die Zählung an
-    private createMarkerForZaehlung(coords: LatLng) {
-        let defaultIcon = new Icon.Default();
-        defaultIcon.options.iconUrl = markerIconRed;
-
-        let options: MarkerOptions = {} as MarkerOptions;
-        options.opacity = 1.0;
-        options.icon = defaultIcon;
-        options.draggable = false;
-
-        this.markerZaehlung = new Marker(coords, options);
-
-        this.markerZaehlung.on("dragend", () => {
-            if (this.markerZaehlung)
-                this.updateZaehlungCoords(this.markerZaehlung.getLatLng());
-        });
-
-        this.markerZaehlung.addTo(this.theMap.mapObject);
-    }
-
-    // Wenn ein Marker existiert wird dieser gelöscht
-    private deleteMarkers() {
-        if (this.markerZaehlstelle) {
-            this.theMap.mapObject.removeLayer(this.markerZaehlstelle);
-            this.markerZaehlstelle = null;
+function createBaseLayers(): L.Control.LayersObject {
+    const stadtkarteGesamt = L.tileLayer.wms(
+        "https://geoportal.muenchen.de/geoserver/gsm/wms?",
+        {
+            layers: "gsm:g_stadtkarte_gesamt",
+            className: "Stadtkarte",
+            attribution: mapAttribution,
         }
-        if (this.markerZaehlung) {
-            this.theMap.mapObject.removeLayer(this.markerZaehlung);
-            this.markerZaehlung = null;
-        }
-    }
+    );
 
-    mapReady() {
-        /*
-         * Wenn die Karte in einem Popup eingebettet ist,
-         * verhält diese sich glitchy, weil es zeitlich
-         * mit dem invalidateSize in onResize() kollidiert oder so.
-         */
-        this.resetMap();
-        if (this.showLuftbild) {
-            this.theMap.mapObject.removeControl(control.attribution());
-        } else {
-            this.theMap.mapObject.addControl(
-                control.attribution({
-                    position: "bottomleft",
-                    prefix: "",
-                })
-            );
+    const luftbild = L.tileLayer.wms(
+        "https://geoportal.muenchen.de/geoserver/gsm/wms?",
+        {
+            layers: "gsm:g_luftbild",
+            className: "Luftbild",
+            attribution: mapAttribution,
         }
-    }
+    );
 
-    private updateZaehlungCoords(zaehlungCoords: LatLng) {
-        if (this.editZaehlungMarker && zaehlungCoords) {
-            this.$emit("updateZaehlungCoords", zaehlungCoords);
-        }
+    return {
+        BaseLayer: props.showLuftbild ? luftbild : stadtkarteGesamt,
+    };
+}
+
+function createMarkerForZaehlung(coords: LatLng) {
+    const defaultIcon = new Icon.Default();
+    defaultIcon.options.iconUrl = markerIconRed;
+
+    const options: MarkerOptions = {} as MarkerOptions;
+    options.opacity = 1.0;
+    options.icon = defaultIcon;
+    options.draggable = props.editZaehlungMarker;
+
+    markerZaehlung = new Marker(coords, options);
+
+    markerZaehlung.on("dragend", () => {
+        if (markerZaehlung) updateZaehlungCoords(markerZaehlung.getLatLng());
+    });
+
+    markerZaehlung.addTo(cardmap);
+}
+
+function createMarkerForZaehlstelle(coords: LatLng) {
+    const options: CircleMarkerOptions = {} as CircleMarkerOptions;
+    options.opacity = 1.0;
+    options.radius = 20;
+
+    markerZaehlstelle = new CircleMarker(coords, options);
+
+    markerZaehlstelle.bindTooltip("Zählstelle", {
+        direction: "right",
+        offset: [options.radius, 0],
+    });
+    markerZaehlstelle.addTo(cardmap);
+}
+
+// Wenn ein Marker existiert wird dieser gelöscht
+function deleteMarkers() {
+    if (markerZaehlstelle) {
+        cardmap.removeLayer(markerZaehlstelle);
+        markerZaehlstelle = null;
+    }
+    if (markerZaehlung) {
+        cardmap.removeLayer(markerZaehlung);
+        markerZaehlung = null;
     }
 }
+
+function resetMap() {
+    deleteMarkers();
+    if (props.latLngZaehlstelle) {
+        createMarkerForZaehlstelle(props.latLngZaehlstelle);
+    }
+    if (props.latLngZaehlung) {
+        createMarkerForZaehlung(props.latLngZaehlung);
+    }
+    cardmap.setView(props.latLngZaehlstelle, 16);
+}
+
+const emits = defineEmits<{
+    (e: "updateZaehlungCoords", v: LatLng): void;
+}>();
+
+function updateZaehlungCoords(zaehlungCoords: LatLng) {
+    if (props.editZaehlungMarker && zaehlungCoords) {
+        emits("updateZaehlungCoords", zaehlungCoords);
+    }
+}
+
+const mapStyle: ComputedRef<string> = computed(() => {
+    return `height: ${props.height}; width: ${props.width}; min-height: ${props.minheight}; z-index: 1`;
+});
 </script>
