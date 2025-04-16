6e81dadcb54811fd83cd3a11fac25b15b0352907
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2025-04-16 15:56:09 +0200

    change to composition api

--- frontend/src/components/icons/ZaehlartIcon.vue
+++ frontend/src/components/icons/ZaehlartIcon.vue
@@ -4,78 +4,70 @@
         :color="color"
         :icon="icon.iconPath"
         :tooltip="icon.tooltip"
-    ></base-icon>
+    />
 </template>
-<script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+<script setup lang="ts">
 import BaseIcon from "@/components/icons/TooltipWithIcon.vue";
 import IconOptions from "@/components/icons/IconOptions";
 import Zaehlart from "@/domain/enums/Zaehlart";
+import { computed } from "vue";
 
-@Component({
-    components: {
-        BaseIcon,
-    },
-})
-export default class ZaehlartIcon extends Vue {
-    @Prop({ default: false }) small?: boolean;
-    @Prop({ default: "black" }) color?: string;
-    @Prop() zaehlart!: string;
+interface Props {
+    small?: boolean;
+    color?: string;
+    zaehlart?: string;
+}
 
-    /**
-     * Lädt das richtige MDI Icon aus der Liste.
-     */
-    get icon(): IconOptions {
-        let result = ZaehlartIcon.zaehlartIcons().get(this.zaehlart);
-        if (result === undefined) {
-            result = new IconOptions(
-                "mdi-help-box",
-                "Keine Information zur Zählart"
-            );
-        }
-        return result;
+const props = withDefaults(defineProps<Props>(), {
+    small: false,
+    color: "black",
+    zaehlart: "",
+});
+
+/**
+ * Lädt das richtige MDI Icon aus der Liste.
+ */
+const icon = computed<IconOptions>(() => {
+    let result = zaehlartIcons().get(props.zaehlart);
+    if (result === undefined) {
+        result = new IconOptions(
+            "mdi-help-box",
+            "Keine Information zur Zählart"
+        );
     }
+    return result;
+});
 
-    /**
-     * Alle Zählarten Icons zu den Schlüsseln
-     */
-    static zaehlartIcons(): Map<string, IconOptions> {
-        return new Map([
-            [Zaehlart.N, new IconOptions("$artN", "Standardzählung")],
-            [
-                Zaehlart.H,
-                new IconOptions(
-                    "$artH",
-                    "Hauptverkehrsrichtung/Oberfläche/Hoch"
-                ),
-            ],
-            [Zaehlart.Q, new IconOptions("$artQ", "Querschnitt")],
-            [Zaehlart.QB, new IconOptions("$artQB", "Bahnschnitt")],
-            [
-                Zaehlart.QH,
-                new IconOptions(
-                    "$artQH",
-                    "Querschnitt/Hauptverkehrsrichtung/Oberfläche/Hoch"
-                ),
-            ],
-            [Zaehlart.QI, new IconOptions("$artQI", "Isarschnitt")],
-            [Zaehlart.QR, new IconOptions("$artQR", "Querschnitt Radverkehr")],
-            [Zaehlart.QS, new IconOptions("$artQSt", "Stadtgrenzzählung")],
-            [
-                Zaehlart.Q_,
-                new IconOptions("$artQS", "Querschnitt/Sonderzählung"),
-            ],
-            [
-                Zaehlart.QT,
-                new IconOptions(
-                    "$artQT",
-                    "Querschnitt Tunnel/Unterführung/Tief"
-                ),
-            ],
-            [Zaehlart.R, new IconOptions("$artR", "Radverkehrszählung")],
-            [Zaehlart.T, new IconOptions("$artT", "Tunnel/Unterführung/Tief")],
-            [Zaehlart.TK, new IconOptions("$artTK", "Teilknoten")],
-        ]);
-    }
+/**
+ * Alle Zählarten Icons zu den Schlüsseln
+ */
+function zaehlartIcons(): Map<string, IconOptions> {
+    return new Map([
+        [Zaehlart.N, new IconOptions("$artN", "Standardzählung")],
+        [
+            Zaehlart.H,
+            new IconOptions("$artH", "Hauptverkehrsrichtung/Oberfläche/Hoch"),
+        ],
+        [Zaehlart.Q, new IconOptions("$artQ", "Querschnitt")],
+        [Zaehlart.QB, new IconOptions("$artQB", "Bahnschnitt")],
+        [
+            Zaehlart.QH,
+            new IconOptions(
+                "$artQH",
+                "Querschnitt/Hauptverkehrsrichtung/Oberfläche/Hoch"
+            ),
+        ],
+        [Zaehlart.QI, new IconOptions("$artQI", "Isarschnitt")],
+        [Zaehlart.QR, new IconOptions("$artQR", "Querschnitt Radverkehr")],
+        [Zaehlart.QS, new IconOptions("$artQSt", "Stadtgrenzzählung")],
+        [Zaehlart.Q_, new IconOptions("$artQS", "Querschnitt/Sonderzählung")],
+        [
+            Zaehlart.QT,
+            new IconOptions("$artQT", "Querschnitt Tunnel/Unterführung/Tief"),
+        ],
+        [Zaehlart.R, new IconOptions("$artR", "Radverkehrszählung")],
+        [Zaehlart.T, new IconOptions("$artT", "Tunnel/Unterführung/Tief")],
+        [Zaehlart.TK, new IconOptions("$artTK", "Teilknoten")],
+    ]);
 }
 </script>
--- frontend/src/components/icons/ZaehldauerIcon.vue
+++ frontend/src/components/icons/ZaehldauerIcon.vue
@@ -4,64 +4,65 @@
         :color="color"
         :icon="icon.iconPath"
         :tooltip="icon.tooltip"
-    ></base-icon>
+    />
 </template>
-<script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+<script setup lang="ts">
 import BaseIcon from "@/components/icons/TooltipWithIcon.vue";
 import IconOptions from "@/components/icons/IconOptions";
 import Zaehldauer from "@/domain/enums/Zaehldauer";
+import { computed } from "vue";
 
-@Component({
-    components: {
-        BaseIcon,
-    },
-})
-export default class ZaehldauerIcon extends Vue {
-    @Prop({ default: false }) small?: boolean;
-    @Prop({ default: "black" }) color?: string;
-    @Prop() zaehldauer!: string;
+interface Props {
+    small?: boolean;
+    color?: string;
+    zaehldauer?: string;
+}
 
-    /**
-     * Lädt das richtige Zähldauer Icon aus der Liste.
-     */
-    get icon(): IconOptions {
-        let result = ZaehldauerIcon.zaehldauerIcons().get(this.zaehldauer);
-        if (result === undefined) {
-            result = new IconOptions(
-                "mdi-help-box",
-                "Keine Information zur Zähldauer"
-            );
-        }
-        return result;
+const props = withDefaults(defineProps<Props>(), {
+    small: false,
+    color: "black",
+    zaehldauer: "",
+});
+
+/**
+ * Lädt das richtige MDI Icon aus der Liste.
+ */
+const icon = computed<IconOptions>(() => {
+    let result = zaehldauerIcons().get(props.zaehldauer);
+    if (result === undefined) {
+        result = new IconOptions(
+            "mdi-help-box",
+            "Keine Information zur Zähldauer"
+        );
     }
+    return result;
+});
 
-    /**
-     * Alle Zähldauer Icons zu den Schlüsseln.
-     */
-    static zaehldauerIcons(): Map<string, IconOptions> {
-        return new Map([
-            [
-                Zaehldauer.DAUER_2_X_4_STUNDEN,
-                new IconOptions("$zaehldauer2x4h", "2x4h Zählung"),
-            ],
-            [
-                Zaehldauer.DAUER_24_STUNDEN,
-                new IconOptions("$zaehldauer24h", "24h Zählung"),
-            ],
-            [
-                Zaehldauer.DAUER_16_STUNDEN,
-                new IconOptions("$zaehldauer16h", "16h Zählung"),
-            ],
-            [
-                Zaehldauer.DAUER_13_STUNDEN,
-                new IconOptions("$zaehldauer13h", "13h Zählung"),
-            ],
-            [
-                Zaehldauer.SONSTIGE,
-                new IconOptions("$zaehldauerSoZ", "Sonstige Zählung"),
-            ],
-        ]);
-    }
+/**
+ * Alle Zähldauer Icons zu den Schlüsseln.
+ */
+function zaehldauerIcons(): Map<string, IconOptions> {
+    return new Map([
+        [
+            Zaehldauer.DAUER_2_X_4_STUNDEN,
+            new IconOptions("$zaehldauer2x4h", "2x4h Zählung"),
+        ],
+        [
+            Zaehldauer.DAUER_24_STUNDEN,
+            new IconOptions("$zaehldauer24h", "24h Zählung"),
+        ],
+        [
+            Zaehldauer.DAUER_16_STUNDEN,
+            new IconOptions("$zaehldauer16h", "16h Zählung"),
+        ],
+        [
+            Zaehldauer.DAUER_13_STUNDEN,
+            new IconOptions("$zaehldauer13h", "13h Zählung"),
+        ],
+        [
+            Zaehldauer.SONSTIGE,
+            new IconOptions("$zaehldauerSoZ", "Sonstige Zählung"),
+        ],
+    ]);
 }
 </script>
