18fcd5d8e4e058eb489ce58f34c2285691e88447
martin.dietrich <martin.dietrich@muenchen.de>
2026-01-16 11:56:15 +0100

    datenportal-header predefined empty

--- frontend/src/util/DefaultObjectCreator.ts
+++ frontend/src/util/DefaultObjectCreator.ts
@@ -58,7 +58,7 @@ export default class DefaultObjectCreator {
 
   public static createDefaultTenantConfigurationDTO(): TenantConfigurationDTO {
     return {
-      datenportalHeader: "Datenportal",
+      datenportalHeader: "",
       mapConfiguration: this.createDefaultMapConfigurationDTO(),
     };
   }
