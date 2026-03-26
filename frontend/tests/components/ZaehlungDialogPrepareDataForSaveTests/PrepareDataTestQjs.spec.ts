3ce20ae6b5bdf0cb7dc84b93e204ce8da9e69706
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2026-03-26 12:00:48 +0100

    fix zaehlart test

--- frontend/tests/components/ZaehlungDialogPrepareDataForSaveTests/PrepareDataTestQjs.spec.ts
+++ frontend/tests/components/ZaehlungDialogPrepareDataForSaveTests/PrepareDataTestQjs.spec.ts
@@ -62,7 +62,7 @@ describe("prepareForSaveZaehlungQjs", () => {
       entityVersion: 0,
       createdTime: "0",
       datum: "2026-03-10",
-      zaehlart: Zaehlart.FJS,
+      zaehlart: Zaehlart.QJS,
       punkt: { lat: "0", lon: "0" },
       projektNummer: "P123",
       projektName: "Project Name",
