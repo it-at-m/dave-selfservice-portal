ae94a79cbf7c001fe524385f6074ef28c2ee5bae
jan-niklasml <jan-niklas.mueller@muenchen.de>
2026-05-06 13:33:36 +0200

    FV-315 und FV-34 Fehler Übersichtskarte und Konfigurative Einbindung der Kartenlayer (#208)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -52,14 +52,18 @@
 </template>
 
 <script setup lang="ts">
+import type MapConfigDTO from "@/types/karte/MapConfigDTO";
+
 import { ref } from "vue";
 import { useRoute } from "vue-router";
 
+import MapConfigService from "@/api/service/MapConfigService";
 import SsoUserInfoService from "@/api/service/SsoUserInfoService";
 import VersionInfoService from "@/api/service/VersionInfoService";
 import TheSnackbar from "@/components/common/TheSnackbar.vue";
 import SsoUserInfoResponse from "@/domain/SsoUserInfoResponse";
 import VersionInfoResponse from "@/domain/VersionInfoResponse";
+import { useMapConfigStore } from "@/store/MapConfigStore";
 import { useSnackbarStore } from "@/store/SnackbarStore";
 import { useUserStore } from "@/store/UserStore";
 
@@ -74,6 +78,7 @@ const frontendVersion = ref<string>("");
 const userStore = useUserStore();
 const route = useRoute();
 const snackbarStore = useSnackbarStore();
+const mapConfigStore = useMapConfigStore();
 
 created();
 
@@ -102,6 +107,9 @@ function created() {
     .catch(() => {
       backendVersion.value = "error";
     });
+  MapConfigService.getMapConfig().then((res: MapConfigDTO) => {
+    mapConfigStore.setMapConfig(res);
+  });
 }
 
 function navigateToHandbuch() {
--- frontend/src/util/DefaultObjectCreator.ts
+++ frontend/src/util/DefaultObjectCreator.ts
@@ -1,3 +1,4 @@
+import type MapConfigDTO from "@/types/karte/MapConfigDTO";
 import type ZaehlungDTO from "@/types/zaehlung/ZaehlungDTO";
 
 import { LatLng } from "leaflet";
@@ -27,5 +28,16 @@ export default class DefaultObjectCreator {
     zaehlung.kreisverkehr = false;
     zaehlung.sonderzaehlung = false;
     return zaehlung;
+  }
+
+  public static createDefaultMapConfigDTO(): MapConfigDTO {
+    return {
+      // München Zentrum
+      lat: "48.137227",
+      lng: "11.575517",
+      zoom: 12,
+      baseLayers: [],
+      overlayLayers: [],
+    };
   }
 }
