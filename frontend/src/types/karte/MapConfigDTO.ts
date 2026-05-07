dd8766af5e6bc77353deeed63eb45e5a22eeb0d6
jan-niklasml <jan-niklas.mueller@muenchen.de>
2026-05-07 10:02:08 +0200

    remove MapConfigDTO

--- frontend/src/types/karte/MapConfigDTO.ts
+++
@@ -1,9 +0,0 @@
-import type LayerDTO from "@/types/karte/LayerDTO";
-
-export default interface MapConfigDTO {
-  lat: string;
-  lng: string;
-  zoom: number;
-  baseLayers: LayerDTO[];
-  overlayLayers: LayerDTO[];
-}
