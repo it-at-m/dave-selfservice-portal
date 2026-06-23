be96077ccc03c6f68308ef529672b8710d7ea20c
Marina Schmoll <marina.schmoll@muenchen.de>
2026-06-23 12:58:32 +0200

    set map height to 100%

--- frontend/src/components/zaehlung/form/KnotenLageForm.vue
+++ frontend/src/components/zaehlung/form/KnotenLageForm.vue
@@ -12,7 +12,7 @@
           sm="9"
         >
           <zaehlung-card-map
-            height="230px"
+            height="100%"
             width="100%"
             :lat-lng-zaehlstelle="coordsZaehlstelle"
             :lat-lng-zaehlung="coordsZaehlung"
