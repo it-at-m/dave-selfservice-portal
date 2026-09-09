c52901dd21cd3e90f7d6db4ca12db5a4727a590b
sfi2022 <109145215+sfi2022@users.noreply.github.com>
2026-09-09 09:58:53 +0200

    User manual files corrected.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -68,7 +68,7 @@ import { useSnackbarStore } from "@/store/SnackbarStore";
 import { useUserStore } from "@/store/UserStore";
 
 const URL_HANDBUCH_LINK =
-    "https://it-at-m.github.io/dave/de/DAVe_Anwenderhandbuch_Datenportal.pdf";
+    "https://it-at-m.github.io/dave/de/DAVe_Admin_Selfservice_Handbuch-1.pdf";
 
 const loggedInUser = ref<string>("no-security");
 
